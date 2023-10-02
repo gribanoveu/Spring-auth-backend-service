25126b82967fe0810656f21f0b139e555e7f94aa
Евгений Грибанов <gribanoveu@yandex.ru>
2023-10-02 12:24:26 +0300

    add doc and fix generate otp code logic
    Took 38 minutes

--- src/main/java/com/github/gribanoveu/auth/utils/JsonUtils.java
+++ src/main/java/com/github/gribanoveu/auth/utils/JsonUtils.java
@@ -15,6 +15,12 @@ import java.util.concurrent.ThreadLocalRandom;
 @Component
 public class JsonUtils {
 
+    /**
+     * Converts the given DTO object to a JSON string.
+     *
+     * @param  dto  the DTO object to convert
+     * @return      the JSON string representation of the DTO object
+     */
     public String convertDtoToJson(Object dto) {
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.findAndRegisterModules();
@@ -26,10 +32,12 @@ public class JsonUtils {
         return "";
     }
 
+    /**
+     * Generates a random OTP (One-Time Password) code  between 100,000 (inclusive) and 1,000,000 (exclusive).
+     *
+     * @return  the randomly generated OTP code
+     */
     public Integer generateRandomOtpCode() {
-        String NUMBERS = "0123456789";
-        var sb = new StringBuilder();
-        for (int i = 0; i < 6; i++) sb.append(NUMBERS.charAt(ThreadLocalRandom.current().nextInt(NUMBERS.length())));
-        return Integer.valueOf(sb.toString());
+        return ThreadLocalRandom.current().nextInt(100_000, 1_000_000);
     }
 }
