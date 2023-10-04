96676526bd9409119ecd19a4a7d640529dc2be45
Евгений Грибанов <gribanoveu@yandex.ru>
2023-10-04 16:06:39 +0300

    add missing validation to dto
    Took 40 seconds

--- src/main/java/com/github/gribanoveu/auth/controllers/dtos/request/UpdatePermissionDto.java
+++ src/main/java/com/github/gribanoveu/auth/controllers/dtos/request/UpdatePermissionDto.java
@@ -1,11 +1,26 @@
 package com.github.gribanoveu.auth.controllers.dtos.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+import static com.github.gribanoveu.auth.constants.RegexpFormat.PERMISSION_PATTERN;
+import static com.github.gribanoveu.auth.constants.ValidationMessages.PERMISSION_FORMAT_MESSAGE;
+import static com.github.gribanoveu.auth.constants.ValidationMessages.PERMISSION_SIZE_MESSAGE;
+
 /**
  * @author Evgeny Gribanov
  * @version 26.09.2023
  */
 public record UpdatePermissionDto(
+        @NotBlank
+        @Pattern(regexp = PERMISSION_PATTERN, message = PERMISSION_FORMAT_MESSAGE)
+        @Size(max = 30, message = PERMISSION_SIZE_MESSAGE)
         String permissionName,
+
+        @NotBlank
+        @Pattern(regexp = PERMISSION_PATTERN, message = PERMISSION_FORMAT_MESSAGE)
+        @Size(max = 30, message = PERMISSION_SIZE_MESSAGE)
         String newName
 ) {
     @Override
