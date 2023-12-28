b649a79021102b85d9b82174a2bc91ffdd0f3f68
Евгений Грибанов <gribanoveu@yandex.ru>
2023-12-28 12:19:43 +0300

    translate error messages to russian
    Took 35 seconds

--- src/main/java/com/github/gribanoveu/cuddle/dtos/enums/ResponseCode.java
+++ src/main/java/com/github/gribanoveu/cuddle/dtos/enums/ResponseCode.java
@@ -13,200 +13,206 @@ import org.springframework.http.HttpStatus;
 public enum ResponseCode {
     SERVER_ERROR(
             "AUT-100",
-            "Internal server error",
-            "Something went wrong",
+            "Внутренняя ошибка сервера",
+            "Что-то пошло не так",
             HttpStatus.INTERNAL_SERVER_ERROR
     ),
     ACCESS_DENIED(
             "AUT-101",
-            "Access denied",
-            "Please check your credentials and make sure you have the required access rights",
+            "Доступ запрещен",
+            "Пожалуйста, проверьте свои учетные данные и убедитесь, что у вас есть необходимые права доступа",
             HttpStatus.FORBIDDEN
     ),
     UNAUTHORIZED(
             "AUT-102",
-            "Unauthorized access",
-            "Authorization required. Log in to your account to access this page. If you have forgotten your credentials, please use the password recovery feature",
+            "Несанкционированный доступ",
+            "Требуется авторизация. Войдите в свою учетную запись, чтобы получить доступ к этой странице. Если вы забыли свои учетные данные, воспользуйтесь функцией восстановления пароля",
             HttpStatus.UNAUTHORIZED
     ),
     BAD_CREDENTIAL(
             "AUT-103",
-            "Invalid credentials",
-            "Please verify that the information entered is correct and try again. If you have forgotten your credentials, please use the password recovery feature",
+            "Неверные учетные данные",
+            "Пожалуйста, проверьте правильность введенной информации и повторите попытку. Если вы забыли свои учетные данные, воспользуйтесь функцией восстановления пароля",
             HttpStatus.BAD_REQUEST
     ),
     MISSING_PARAM(
             "AUT-104",
-            "Missing required query parameter",
-            "Required query parameter is missing. Please make sure you have entered all required parameters and try again",
+            "Отсутствует необходимый параметр запроса",
+            "Убедитесь, что вы корректно ввели все необходимые параметры, и повторите попытку",
             HttpStatus.BAD_REQUEST
     ),
     TOKEN_NOT_VALID(
             "AUT-105",
-            "Invalid token",
-            "Please make sure you are using a valid token and try again",
+            "Неверный токен",
+            "Убедитесь, что вы используете действительный токен, и повторите попытку",
             HttpStatus.UNAUTHORIZED
     ),
     VALIDATION_ERROR(
             "AUT-106",
-            "Validation error",
-            "Please check that the data is in the correct format and try again",
+            "Ошибка валидации",
+            "Убедитесь, что данные представлены в правильном формате, и повторите попытку",
             HttpStatus.BAD_REQUEST
     ),
     VALIDATION_ERROR_DETAIL(
             "AUT-107",
-            "Validation error",
-            "Field '%s' - %s",
+            "Ошибка валидации",
+            "Поле '%s' - %s",
             HttpStatus.BAD_REQUEST
     ),
     RESOURCE_NOT_FOUND(
             "AUT-108",
-            "Resource not found",
-            "The requested resource could not be found. Please verify that the information entered is correct and try again",
+            "Ресурс не найден",
+            "Запрашиваемый ресурс не найден. Пожалуйста, проверьте правильность введенной информации и повторите попытку",
             HttpStatus.NOT_FOUND
     ),
     ACCOUNT_BANNED(
             "AUT-109",
-            "Account banned",
-            "Your account has been banned due to a violation of the Terms of Use Policy",
+            "Учетная запись заблокирована",
+            "Ваш аккаунт был заблокирован из-за нарушения правил. Подробная информация отправлена на ваш электронный адрес",
             HttpStatus.UNAUTHORIZED
     ),
     ACCOUNT_DISABLED(
             "AUT-110",
-            "Account disabled",
-            "Your account has been disabled contact support to find out why",
+            "Учетная запись заморожена",
+            "Действие вашей учетной записи приостановлено. Подробная информация отправлена на ваш электронный адрес",
             HttpStatus.UNAUTHORIZED
     ),
     NO_RESTRICTIONS(
             "AUT-111",
-            "No restrictions",
-            "There are no restrictions on your account",
+            "Нет ограничений",
+            "На вашей учетной записи нет никаких ограничений",
             HttpStatus.NOT_FOUND
     ),
     PASSWORD_UPDATED(
             "AUT-200",
-            "Password updated",
-            "Password updated successfully",
+            "Пароль обновлен",
+            "Используйте новый пароль для входа в учетную запись",
             HttpStatus.OK
     ),
     PASSWORD_NOT_EQUALS(
             "AUT-201",
-            "Passwords do not match",
-            "The passwords do not match. Please make sure you have entered the same passwords and try again",
+            "Пароли не совпадают",
+            "Убедитесь, что вы ввели одинаковые пароли, и повторите попытку",
             HttpStatus.BAD_REQUEST
     ),
     PASSWORD_EQUALS(
             "AUT-202",
-            "Password equals",
-            "The new password must be different from the old one. Please enter a different password and try again",
+            "Совпадение паролей",
+            "Новый пароль должен отличаться от старого. Пожалуйста, введите другой пароль и повторите попытку",
             HttpStatus.BAD_REQUEST
     ),
     OLD_PASSWORD_NOT_MATCH(
             "AUT-203",
-            "Passwords do not match",
-            "The old password does not match the current password. Please make sure you have entered the correct old password and try again",
+            "Пароли не совпадают",
+            "Старый пароль не совпадает с текущим. Убедитесь, что вы ввели правильный старый пароль, и повторите попытку",
             HttpStatus.BAD_REQUEST
     ),
     OTP_CODE_CREATED(
             "AUT-300",
-            "OTP code created",
-            "The one-time access code (OTP) has been successfully created. Please check your e-mail",
+            "OTP код создан",
+            "Одноразовый код доступа (OTP) был успешно создан. Пожалуйста, проверьте свою электронную почту",
             HttpStatus.OK
     ),
     OTP_CODE_NOT_FOUND(
             "AUT-301",
-            "OTP code not found",
-            "The One-Time Access (OTP) code was not found. Please make sure you have entered the correct code and try again. If you do not have a valid code, please create a new code",
+            "Не найден код OTP",
+            "Код одноразового доступа (OTP) не найден или истек. Убедитесь, что вы ввели правильный код, и повторите попытку. Если у вас нет действующего кода, создайте новый",
             HttpStatus.NOT_FOUND
     ),
     OTP_CODE_EXIST(
             "AUT-302",
-            "OTP code exist",
-            "The one-time access code (OTP) has already been created. Please check your email. If you have not received the code, please wait before creating a new code",
+            "Код OTP уже создан",
+            "Одноразовый код доступа (OTP) уже создан. Пожалуйста, проверьте свою электронную почту. Если вы не получили код, пожалуйста, подождите, прежде чем создавать новый код",
             HttpStatus.TOO_MANY_REQUESTS
     ),
     EMAIL_NOT_SEND(
             "AUT-303",
-            "Error when send email",
-            "An error occurred when sending an email to this email account",
+            "Ошибка при отправке письма",
+            "Произошла ошибка при отправке письма электронной почты, связанной с данной учетной записью",
             HttpStatus.TOO_MANY_REQUESTS
     ),
     USER_CREATED(
             "AUT-400",
-            "User created",
-            "User has been successfully created",
+            "Успешная регистрация",
+            "Учетная запись успешно зарегистрирована. Выполните вход для продолжения",
             HttpStatus.OK
     ),
     USER_DELETED(
             "AUT-401",
-            "User deleted",
-            "User has been successfully deleted",
+            "Пользователь удален",
+            "Пользователь был успешно удален",
             HttpStatus.OK
     ),
     USER_DISABLED(
             "AUT-402",
-            "User disabled",
-            "User has been successfully disabled",
+            "Пользователь отключен",
+            "Учетная запись пользователя приостановила свое действие",
             HttpStatus.OK
     ),
     USER_ENABLED(
             "AUT-403",
-            "User enabled",
-            "User has been successfully enabled",
+            "Пользователь включен",
+            "Действие учетной записи пользователя успешно восстановлено",
             HttpStatus.OK
     ),
     USER_NOT_EXIST(
             "AUT-404",
-            "User not found",
-            "User not found. Please make sure you have entered the correct email and try again",
+            "Пользователь не найден",
+            "Пожалуйста, убедитесь, что вы ввели правильный адрес электронной почты, и повторите попытку",
             HttpStatus.NOT_FOUND
     ),
     USER_UPDATED(
             "AUT-405",
-            "User updated successfully",
-            "User has been successfully updated",
+            "Пользователь успешно обновлен",
+            "Учетная запись пользователя успешно обновлена",
             HttpStatus.OK
     ),
     USER_ALREADY_EXIST(
             "AUT-406",
-            "User already exists",
-            "User already exists. Please make sure you have entered the correct email and try again",
+            "Пользователь уже существует",
+            "Пожалуйста, убедитесь, что вы ввели правильный адрес электронной почты, и повторите попытку",
+            HttpStatus.CONFLICT
+    ),
+    EMAIL_ALREADY_EXIST(
+            "AUT-407",
+            "Электронная почта уже существует",
+            "Пожалуйста, убедитесь, что вы ввели отличный от текущего адрес электронной почты, и повторите попытку",
             HttpStatus.CONFLICT
     ),
     USER_BANNED(
-            "AUT-407",
-            "User banned",
-            "User has been successfully banned",
+            "AUT-408",
+            "Пользователь заблокирован",
+            "Действие учетной записи пользователя заблокировано",
             HttpStatus.OK
     ),
     USER_UN_BANNED(
-            "AUT-408",
-            "User unbanned",
-            "User has been successfully unbanned",
+            "AUT-409",
+            "Пользователь разблокирован",
+            "Ограничения с учетной записи пользователя сняты",
             HttpStatus.OK
     ),
     PERMISSION_UPDATED_MODERATOR(
             "AUT-500",
-            "Permission updated",
-            "The user is now a moderator",
+            "Доступ повышен",
+            "Учетная запись теперь является модератором",
             HttpStatus.OK
     ),
     PERMISSION_UPDATED_USER(
             "AUT-501",
-            "Permission updated",
-            "The user is now a user",
+            "Доступ понижен",
+            "Учетная запись теперь является пользователем",
             HttpStatus.OK
     ),
     SEND_MESSAGE(
             "INF-100",
-            "Information message",
+            "Информация",
             "%s",
             HttpStatus.OK
     ),
     BAN_REASON_NOT_FOUND(
             "MOD-100",
-            "Warning",
-            "Restriction reason not exist or not found",
+            "Информация",
+            "Ограничения на учетной записи не найдены или отсутствуют",
             HttpStatus.OK
     );
 
