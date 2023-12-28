f39f8f94ec4357ca7cd1d6c33d2037b075436285
Евгений Грибанов <gribanoveu@yandex.ru>
2023-12-28 12:21:25 +0300

    translate validation messages to russian
    Took 19 seconds

--- src/main/java/com/github/gribanoveu/cuddle/constants/ValidationMessages.java
+++ src/main/java/com/github/gribanoveu/cuddle/constants/ValidationMessages.java
@@ -5,9 +5,9 @@ package com.github.gribanoveu.cuddle.constants;
  * @version 29.08.2023
  */
 public class ValidationMessages {
-    public static final String EXCEPTION_VIOLATION_PATTERN = "Field '%s' - %s";
-    public static final String NOT_BLANK_EXCEPTION_MESSAGE = "cannot be empty";
-    public static final String PATTERN_EXCEPTION_MESSAGE = "must match the expected format";
-    public static final String SIZE_EXCEPTION_MESSAGE = "size should be no larger than expected";
-    public static final String MINIMAL_AGE_EXCEPTION_MESSAGE = "must be at least 18 years old, or have expected format";
+    public static final String EXCEPTION_VIOLATION_PATTERN = "Поле '%s' - %s";
+    public static final String NOT_BLANK_EXCEPTION_MESSAGE = "не может быть пустым";
+    public static final String PATTERN_EXCEPTION_MESSAGE = "должно соответствовать ожидаемому формату";
+    public static final String SIZE_EXCEPTION_MESSAGE = "размер не должен превышать ожидаемый";
+    public static final String MINIMAL_AGE_EXCEPTION_MESSAGE = "должен быть не моложе 18 лет или иметь ожидаемый формат";
 }
